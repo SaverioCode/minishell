6a6dc765d2331536883cad16101299971264ff37
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-05-12 08:20:42 +0200

    update

--- src/parser/lexical_utils.c
+++ src/parser/lexical_utils.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 04:17:47 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/05/12 07:21:39 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/05/12 08:20:34 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ static int	token_check_first(char old, char new)
 	{
 		if (new == CHAR || new == '(')
 			return (0);
-		if (new == INP || new == INP2 || new == OUT || new == OUT2)
+		if (new == INP || new == HDOC || new == OUT || new == APP)
 			return (0);
 		return (-1);
 	}
-	if (old == INP || old == INP2 || old == OUT || old == OUT2)
+	if (old == INP || old == HDOC || old == OUT || old == APP)
 	{
 		if (new == CHAR)
 			return (0);
-		if (new == INP || new == INP2 || new == OUT || new == OUT2)
+		if (new == INP || new == HDOC || new == OUT || new == APP)
 			return (0);
 		return (-1);
 	}
@@ -44,7 +44,7 @@ static int	token_check_second(char old, char new, int *brkts)
 		*brkts++;
 		if (new == CHAR)
 			return (0);
-		if (new == INP || new == INP2 || new == OUT || new == OUT2)
+		if (new == INP || new == HDOC || new == OUT || new == APP)
 			return (0);
 		return (-1);
 	}
@@ -53,7 +53,7 @@ static int	token_check_second(char old, char new, int *brkts)
 		if (brkts[0] == 0)
 			return (-1);
 		*brkts--;
-		if (new == INP || new == INP2 || new == OUT || new == OUT2)
+		if (new == INP || new == HDOC || new == OUT || new == APP)
 			return (0);
 		if (new == AND || new == PIPE)
 			return (0);
