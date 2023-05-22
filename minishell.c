719d2ae13c5919b4378a298ff805b0e4dfdf46e9
Francesco Saverio Garzi Cosentino <fgarzi-c@e3r11p5.42roma.it>
2023-05-22 11:11:15 +0200

    update

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 11:51:46 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/05/22 11:10:23 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/05/22 11:11:07 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	main(int ac, char **av, char **env)
 	ft_check_input(ac, av);
 	info = NULL;
 	info = ft_calloc(1, sizeof(t_info));
+	init_tinfo(info);
 	info->env = env;
 	// ft_set_starting_env(env, info);  /// problems here /// but check if delete before correct them ///
 	// set_signals();
@@ -30,7 +31,6 @@ int	main(int ac, char **av, char **env)
 		root = NULL;
 		root = ft_calloc(1, sizeof(t_node));
 		init_tnode(root);
-		init_tinfo(info);
 		ft_handle_input(info);
 		ft_parser(root, info->input);
 		// ft_run_all(&root, &info);
