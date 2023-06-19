17dbfe6d2adb906e866122c73c61b4dac5452c52
Francesco Saverio Garzi Cosentino <fgarzi-c@e3r11p5.42roma.it>
2023-06-20 00:32:55 +0200

    update

--- src/ms_set_termios.c
+++ src/ms_set_termios.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 01:02:06 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/06/19 23:33:37 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/06/20 00:32:29 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	ms_set_termios(void)
 		write(1, "ERROR\n", 6);
 		exit(1);
 	}
-	
 	settings.c_cc[VINTR] = 4;
 	settings.c_cc[VEOF] = 3;
+	settings.c_cc[VQUIT] = _POSIX_VDISABLE;
 	if (tcsetattr(fd, TCSANOW, &settings) != 0)
 	{
 		write(1, "ERROR2\n", 7);
