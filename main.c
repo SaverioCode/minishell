5654f4ee8f17983759f2dec8f763b98c6f549e55
Francesco Saverio Garzi Cosentino <fgarzi-c@e3r11p5.42roma.it>
2023-07-15 23:37:19 +0200

    updaet

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 11:51:46 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/07/15 23:22:57 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/07/15 23:36:54 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(int ac, char **av, char **env)
 	ms_get_prompt_str(info);
 	ms_print_header();
 	while (1)
-	{ 
+	{
 		root = ft_calloc(1, sizeof(t_node));
 		ms_init_tnode(root);
 		info->root = root;
@@ -33,7 +33,7 @@ int	main(int ac, char **av, char **env)
 		free(info->input);
 		info->input = NULL;
 		ms_execute_tree(root, info);
-		info->status = 0; /// to modify in order to have access to variable $? with last exit status /// 
+		info->status = 0;
 		ms_free(root, NULL);
 	}
 	return (0);
